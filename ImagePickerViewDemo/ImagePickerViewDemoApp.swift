70cab4c3f8d896c851a15ecf55a12518df35d7cd
Alex Nagy <therebeloper@gmail.com>
2021-01-19 09:58:09 +0200

    Initial Commit

---
+++ ImagePickerViewDemo/ImagePickerViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagePickerViewDemoApp.swift
+//  ImagePickerViewDemo
+//
+//  Created by Alex Nagy on 19.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct ImagePickerViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
